c3a9bda90ce19ea9569f2d7d8bc49248c97c6975
吴毓麟 <912426167@qq.com>
2020-07-02 16:50:07 +0800

    UDP聊天窗口的实现

---
+++ UdpMsg/widget.cpp
@@ -0,0 +1,43 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QHostAddress>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    udpSocket = new QUdpSocket(this);
+    udpSocket->bind(QHostAddress::Any,8888);   //这个绑定其实是设置自己的端口号
+    connect(udpSocket,SIGNAL(readyRead()),this,SLOT(RcvData()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+void Widget::RcvData()
+{
+    char buf[1024] = {0};
+    QHostAddress address ;
+    quint16 port;
+    quint64 len = udpSocket->readDatagram(buf,sizeof(buf),&address,&port);
+    if(len>0)
+    {
+        QString str = QString("[%1:%2]:%3").arg(address.toString()).arg(port).arg(buf);
+        ui->textEdit_rcv->append(str);
+    }
+}
+void Widget::on_pushButton_send_clicked()
+{
+    QHostAddress ip = (QHostAddress)ui->lineEdit_ip->text();
+    quint16 port = ui->lineEdit_port->text().toInt();
+    QString str =  ui->textEdit_send->toPlainText();
+    udpSocket->writeDatagram(str.toUtf8().data(),ip,port);
+    ui->textEdit_rcv->append(str);
+}
+
+void Widget::on_pushButton_close_clicked()
+{
+    this->close();
+}
---
+++ UdpMsg/widget.h
@@ -0,0 +1,31 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QUdpSocket>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+private slots:
+    void RcvData();
+
+    void on_pushButton_send_clicked();
+
+    void on_pushButton_close_clicked();
+
+private:
+    Ui::Widget *ui;
+
+    QUdpSocket *udpSocket;
+};
+
+#endif // WIDGET_H
